cbb2a1698576f2a2579b13dc42af7617cc3c74b7
gz-hejiehui <gz-hejiehui@outlook.com>
2023-02-03 13:23:10 +0000

    Create the tag's resource page

---
+++ app/Filament/Resources/TagResource.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TagResource\Pages;
+use App\Filament\Resources\TagResource\RelationManagers;
+use App\Models\Tag;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TagResource extends Resource
+{
+    protected static ?string $model = Tag::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->label(__('fields.name'))
+                    ->required()
+                    ->rules(['max:20'])
+                    ->unique(table: Tag::class, ignoreRecord: true),
+                Forms\Components\TextInput::make('description')
+                    ->label(__('fields.description'))
+                    ->rules(['max:50'])
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')->label(__('fields.name')),
+                Tables\Columns\TextColumn::make('description')->label(__('fields.description')),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageTags::route('/'),
+        ];
+    }
+}
