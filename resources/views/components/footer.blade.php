33e3538f1b4addd8a788920653c7c8f602108952
gz-hejiehui <gz-hejiehui@outlook.com>
2023-02-05 15:01:34 +0800

    feat: Add the scroll to top button

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -6,8 +6,11 @@
             <a href="https://blog.xboxnow.net">{{ config('app.name', 'Laravel') }}</a>
         </strong>
         . 保留一切权利。
-
         <a href="http://beian.miit.gov.cn/">粤ICP备2021345676号</a>
-    </div>
+
+        <a href="#top" class="o-scroll-up float-end" title="back to top">
+            <span class="o-scroll-up-text hidden-sm-down">Back to top</span>
+            <span class="o-scroll-up-icon" aria-hidden="true"></span>
+        </a>
     </div>
 </footer>
